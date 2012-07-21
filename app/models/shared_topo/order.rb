5fea53fdf6f728d89ee5b01e3c1f8bdc8091c581
David Elam <delam@paynearme.com>
2012-07-20 19:45:58 -0700

    add order

--- app/models/shared_topo/order.rb
+++ app/models/shared_topo/order.rb
@@ -2,6 +2,7 @@ module SharedTopo
   class Order < ActiveRecord::Base
     establish_connection "topo"
     set_table_name :orders
+    self.inheritance_column = nil
     # attr_accessible :title, :body
   end
 end
